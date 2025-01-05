fee25a9f59954eb3656d35dbb2ea3be31ec89c49
Lee Korff-Korn <Leekk270@gmail.com>
2025-01-05 15:32:12 -0500

    added console to inspect remote delivery of users

--- app/data/page.tsx
+++ app/data/page.tsx
@@ -4,24 +4,19 @@ import { users, addresses } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
 export default async function DataPage() {
-	const db = await getDb();
-	const allUsers = await db
-		.select()
-		.from(users)
-		.leftJoin(addresses, eq(users.id, addresses.userId));
+  const db = await getDb();
+  const allUsers = await db.select().from(users).leftJoin(addresses, eq(users.id, addresses.userId));
+  console.log(allUsers);
+  // Transform joined data to match User type
+  const transformedUsers = allUsers.map(({ users: user, addresses: address }) => ({
+    ...user,
+    address: address,
+  }));
 
-	// Transform joined data to match User type
-	const transformedUsers = allUsers.map(
-		({ users: user, addresses: address }) => ({
-			...user,
-			address: address,
-		})
-	);
-
-	return (
-		<div className="container mx-auto py-4">
-			<h1 className="text-2xl font-bold mb-4">Users</h1>
-			<UsersDataTable users={transformedUsers} />
-		</div>
-	);
+  return (
+    <div className="container mx-auto py-4">
+      <h1 className="text-2xl font-bold mb-4">Users</h1>
+      <UsersDataTable users={transformedUsers} />
+    </div>
+  );
 }
